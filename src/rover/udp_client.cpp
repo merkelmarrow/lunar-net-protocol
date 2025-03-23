246a8f44113ba338b57b8be4d34e53dca33e40b7
merkelmarrow <marco.a.blackwell@gmail.com>
2025-03-23 01:31:28 +0000

    feat: implement UdpClient destructor

--- src/rover/udp_client.cpp
+++ src/rover/udp_client.cpp
@@ -1,8 +1,24 @@
 // src/rover/udp_client.cpp
 
-#include "udp_client.hpp"
+#include <iostream>
+
 #include <boost/asio/io_context.hpp>
+
+#include "udp_client.hpp"
 
 UdpClient::UdpClient(boost::asio::io_context &io_context)
     : io_context_(io_context), socket_(io_context, udp::endpoint(udp::v4(), 0)),
       running_(false) {}
+
+UdpClient::~UdpClient() {
+  running_ = false;
+
+  if (socket_.is_open()) {
+    boost::system::error_code ec;
+    socket_.close(ec);
+    if (ec) {
+      std::cerr << "[ERROR] Error closing socket: " << ec.message()
+                << std::endl;
+    }
+  }
+}
