c6d61c8a0e7078f5f355bbc552dd0c7c1f940c15
merkelmarrow <marco.a.blackwell@gmail.com>
2025-03-30 02:00:07 +0100

    feat: begin defining LumenPacket structure

--- include/common/lumen_packet.hpp
+++ include/common/lumen_packet.hpp
@@ -1,1 +1,24 @@
 // include/common/lumen_packet.hpp
+
+#pragma once
+
+#include "lumen_header.hpp"
+#include <cstdint>
+#include <optional>
+#include <vector>
+
+class LumenPacket {
+public:
+  // create a new packet
+  LumenPacket(const LumenHeader &header, const std::vector<uint8_t> &payload);
+
+  // parse packet from raw bytes
+  static std::optional<LumenPacket>
+  from_bytes(const std::vector<uint8_t> &bytes);
+
+  std::vector<uint8_t> to_bytes() const;
+
+private:
+  LumenHeader header_;
+  std::vector<uint8_t> payload_;
+};
